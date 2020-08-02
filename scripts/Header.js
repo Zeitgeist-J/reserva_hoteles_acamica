376500c689b921ceeda339dd66219a40a9307c86
Nicolás Macias <nicolas.macias@creci.co>
2020-08-02 11:54:25 -0500

    header fixed

--- scripts/Header.js
+++ scripts/Header.js
@@ -1,4 +1,4 @@
-const diasSemana = 'Lunes_Martes_Miércoles_Jueves_Viernes_Sábado_Domingo'.split('_');
+const diasSemana = 'Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado'.split('_');
 
 const meses = 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_');
 
@@ -6,7 +6,7 @@ const getDiaSemana = (index) => diasSemana[index];
 
 const getMes = (index) => meses[index];
 
-const buildDateFormat = (date) => `${getDiaSemana(Number(date.format('d')) - 1)},
+const buildDateFormat = (date) => `${getDiaSemana(Number(date.format('d')))},
     ${date.format('D')} de ${getMes(Number(date.format('M')) - 1)}
     de ${date.format('YYYY')}`;
 
